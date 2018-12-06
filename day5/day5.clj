ede238cfbc624613f8341997265bff38625c0e55
Mario <mario.cordova.862@my.csun.edu>
2018-12-05 21:45:08 -0800

    Final day 5

--- day5/day5.clj
+++ day5/day5.clj
@@ -6,22 +6,50 @@
     (clojure.string/replace polymer "\n" "")))
 
 #_(def input "dabAcCaCBAcCcaDA")
+#_(def input "bAAacCbe")
 
-(defn part-one
+(defn find-next-unit
   ""
-  []
-  (count (find-filter-remove)))
+  [polymer]
+  (loop [polymer polymer
+         matches #{}]
+    (if (or (not (empty? matches)) (nil? (seq polymer)))
+      matches
+      (let [units (re-find #"(?i)([a-z])\1" polymer)
+            units (first units) 
+            matches (if (= (first units) (second units))
+                      matches
+                      (conj matches units))]
+        (recur (second (clojure.string/split polymer (re-pattern (str (first units))) 2)) 
+               matches)))))
 
 (defn find-filter-remove
   ""
-  []
+  [input]
   (loop [poly input
-         unit-to-be-removed (first (filter (fn [unit] (not (= (first unit) (second unit))))
-                                           (map #(first %) (re-seq #"(?i)([a-z])\1"  poly))))]
-    (if (nil? unit-to-be-removed)
+         unit-to-be-removed (find-next-unit poly)]
+    (if (= 0 (count unit-to-be-removed))
       poly
-      (let [result (clojure.string/replace-first poly unit-to-be-removed "")]
+      (let [result (clojure.string/replace poly (first unit-to-be-removed) "")]
         (recur result
-               (first (filter (fn [unit] (not (= (first unit) (second unit))))
-                              (map #(first %) (re-seq #"(?i)([a-z])\1"  result)))))))))
+               (find-next-unit result))))))
 
+(defn part-one-l
+  ""
+  [input]
+  (count (find-filter-remove input)))
+
+(def part-one
+  (memoize part-one-l))
+
+(defn part-two
+  ""
+  []
+  (let [alphabet (map char (range 97 123))
+        polymer input]
+    (pmap (fn [letter]
+                    (let [stripped-polymer (clojure.string/replace polymer
+                                                                   (re-pattern (str "(?i)" letter))
+                                                                   "")]
+                      (part-one stripped-polymer)))
+                  alphabet)))
